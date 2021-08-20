baaefdab025fd7db45d71e3acd464b17879ea0ba
fpetrovicc <filippetrovic603@gmail.com>
2021-08-20 20:31:07 +0200

    feat: implemented font/characters table and loading them in memory

---
+++ src/chip8.cpp
@@ -0,0 +1,102 @@
+#include "chip8.hpp"
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <random>
+#include <chrono>
+
+const unsigned int FONTSET_SIZE = 80;
+const unsigned int FONTSET_START_ADDRESS = 0x50;
+const unsigned int START_ADDRESS = 0x200;
+
+// Fontset (16 chars @ 5 bytes each = 80 byte array)
+uint8_t fontset[FONTSET_SIZE] =
+        {
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
+
+Chip8::Chip8()
+        : randGen(std::chrono::system_clock::now().time_since_epoch().count())
+{
+    // Initialization of Program Counter
+    pc = START_ADDRESS;
+
+    // Loading the fonts (chars) into the memory
+    for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+    {
+        memory[FONTSET_START_ADDRESS + i] = fontset[i];
+    }
+
+    // Initialization of Random Number Generator (RNG)
+    randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
+
+    // Setting up function pointer table
+    // Table
+    table[0x0] = &Chip8::Table0;
+    table[0x1] = &Chip8::OP_1nnn;
+    table[0x2] = &Chip8::OP_2nnn;
+    table[0x3] = &Chip8::OP_3xkk;
+    table[0x4] = &Chip8::OP_4xkk;
+    table[0x5] = &Chip8::OP_5xy0;
+    table[0x6] = &Chip8::OP_6xkk;
+    table[0x7] = &Chip8::OP_7xkk;
+    table[0x8] = &Chip8::Table8;
+    table[0x9] = &Chip8::OP_9xy0;
+    table[0xA] = &Chip8::OP_Annn;
+    table[0xB] = &Chip8::OP_Bnnn;
+    table[0xC] = &Chip8::OP_Cxkk;
+    table[0xD] = &Chip8::OP_Dxyn;
+    table[0xE] = &Chip8::TableE;
+    table[0xF] = &Chip8::TableF;
+
+    // Table 0
+    table0[0x0] = &Chip8::OP_00E0;
+    table0[0xE] = &Chip8::OP_00EE;
+
+    // Table 8
+    table8[0x0] = &Chip8::OP_8xy0;
+    table8[0x1] = &Chip8::OP_8xy1;
+    table8[0x2] = &Chip8::OP_8xy2;
+    table8[0x3] = &Chip8::OP_8xy3;
+    table8[0x4] = &Chip8::OP_8xy4;
+    table8[0x5] = &Chip8::OP_8xy5;
+    table8[0x6] = &Chip8::OP_8xy6;
+    table8[0x7] = &Chip8::OP_8xy7;
+    table8[0xE] = &Chip8::OP_8xyE;
+
+    // Table E
+    tableE[0x1] = &Chip8::OP_ExA1;
+    tableE[0xE] = &Chip8::OP_Ex9E;
+
+    // Table F
+    tableF[0x07] = &Chip8::OP_Fx07;
+    tableF[0x0A] = &Chip8::OP_Fx0A;
+    tableF[0x15] = &Chip8::OP_Fx15;
+    tableF[0x18] = &Chip8::OP_Fx18;
+    tableF[0x1E] = &Chip8::OP_Fx1E;
+    tableF[0x29] = &Chip8::OP_Fx29;
+    tableF[0x33] = &Chip8::OP_Fx33;
+    tableF[0x55] = &Chip8::OP_Fx55;
+    tableF[0x65] = &Chip8::OP_Fx65;
+}
+
+// TODO - Implement LoadROM function
+
+// TODO - Implement Cycle function
+
+// TODO - Implement table/opcodes
