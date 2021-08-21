fc59dd6a708b2b55a1a3b3145e9f374ebc9410c9
fpetrovicc <filippetrovic603@gmail.com>
2021-08-21 23:31:40 +0200

    info: everything working, basic games can be played & all tests are pass

--- src/Platform.cpp
+++ src/Platform.cpp
@@ -5,7 +5,7 @@ Platform::Platform(char const* title, int windowWidth, int windowHeight, int tex
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow(title, 0, 0, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
 }
